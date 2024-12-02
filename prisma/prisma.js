03243d958febca30900fc48c6328c7b73c755cd6
Raul Gamero <raul.a.gamero@gmail.com>
2024-12-02 21:07:00 +0100

    testing nodemon dev

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -2,3 +2,4 @@ const { PrismaClient } = require('@prisma/client'); //importamos PrismaClient de
 const prisma = new PrismaClient(); //instanciamos prisma
 
 module.exports = prisma; //exportamos la instancia de prisma
+
